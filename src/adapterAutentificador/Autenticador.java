f8a2b10fe840800c33537f4df098fc3ff2e110dc
Alejandro Susmelj <110204705+alesusmelj@users.noreply.github.com>
2023-06-10 14:07:17 -0300

    Archivos Java de correciones en sector Usuarios

--- src/adapterAutentificador/Autenticador.java
+++ src/adapterAutentificador/Autenticador.java
@@ -1,7 +1,6 @@
 package adapterAutentificador;
 
 import java.util.*;
-
 import modelo.EtipoUsuario;
 import modelo.Usuario;
 import testMain.Utilidades;
@@ -9,36 +8,44 @@ import testMain.Utilidades;
 public class Autenticador implements IAdapterAutenticador {
 
     public Autenticador() {
-    }
+	}
+
     public void autenticar(String usuario, String password, EtipoUsuario tipoUsuario) {
+
     	boolean iniciadoAux = false;
     	int contadorAux = 0;
-    	System.out.println("Iniciando sesiÛn...");
+    	System.out.println("Iniciando sesi√≥n...");
     	Utilidades.esperar(1);
+
     	for(Usuario u:Utilidades.getUsuarios()) {
     		contadorAux++;
-    	if(iniciadoAux == false) {
-	    		if(u.isAutenticado() == false) {
-		    		if(u.getNombreUsuario() == usuario && u.getPassword() == password) {
-		    			if(u.isAutenticado() == false) {
-			    			System.out.println("El usuario: "+usuario+"("+tipoUsuario+")"+" ha iniciado sesiÛn correctamente.");
-			    			u.setAutenticado(true);
-			    			iniciadoAux = true;
-		    			}
-		    			else if(u.isAutenticado() == true) {
-		    				System.out.println("El usuario ya est· conectado");
-		    			}
-		    		}
-		    		else if(Utilidades.getUsuarios().size() == contadorAux && u.isAutenticado() == false) {
-		    			System.out.println("El usuario o la contraseÒa es incorrecta");
-		    		}
-	    		}
-	    		else if(u.isAutenticado() == true) {
-	    			System.out.println("No es posible iniciar sesiÛn, ya hay una sesiÛn activa.");
-	    			iniciadoAux = true;
-	    		}
-	    	}
-    	}
 
+			if(iniciadoAux == false) {
+				
+				if(u.isAutenticado() == false) {
+					if(u.getNombreUsuario() == usuario && u.getPassword() == password) {
+						if(u.isAutenticado() == false) {
+							
+							System.out.println("El usuario: " + usuario + " (" + tipoUsuario + ")" + " ha iniciado sesi√≥n correctamente.");
+							u.setAutenticado(true);
+							iniciadoAux = true;
+						}
+
+						else if(u.isAutenticado() == true) {
+							System.out.println("El usuario ya est√° conectado.");
+						}
+					}
+
+					else if(Utilidades.getUsuarios().size() == contadorAux && u.isAutenticado() == false) {
+						System.out.println("El usuario o la contrase√±a es incorrecta.");
+					}
+				}
+
+				else if(u.isAutenticado() == true) {
+					System.out.println("No es posible iniciar sesi√≥n, ya hay una sesi√≥n activa.");
+					iniciadoAux = true;
+				}
+			}
+    	}
     }
 }
