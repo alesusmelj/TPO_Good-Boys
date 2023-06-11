a919e3fa1a4293feb392255dad1bf4a505f10c56
Vanesa Aldana Romero <vanesahabbo07@gmail.com>
2023-06-11 18:15:10 -0300

    agregado de funcionalidad para cambiar el estaod del animal desde atenderTratamiento

--- src/estadoCondicionMedica/Enfermo.java
+++ src/estadoCondicionMedica/Enfermo.java
@@ -5,11 +5,11 @@ import modelo.Animal;
 public class Enfermo implements EstadoCondicionMedica {
 
     public void recuperar(Animal animal) {
-        System.out.println("El animal se recuperó.");
+        System.out.println("El animal se ha recuperado.");
         animal.cambiarEstado(new Saludable());
     }
 
     public void serAdoptado(Animal animal) {
-        System.out.println("El animal no puede ser adoptado, debe recuperarse antes.");
+        System.out.println("El animal no puede ser adoptado, debe estar saludable.");
     }
 }
